f37a6ebdbb3534aeabc0f9e603e55e82c9ddfaff
Ebram Youssef <ebramnimr2016@gmail.com>
2025-06-30 10:57:55 -0700

    initialization

---
+++ main.py
@@ -0,0 +1,10 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+import pydm
+from utils import DesignerDisplay
+import pyqtgraph as pg
+
+class Viewer(DesignerDisplay, QtWidgets.QWidget):
+    filname = "uitils/viewer.ui"
+
+    def __init_(self):
+        pass
